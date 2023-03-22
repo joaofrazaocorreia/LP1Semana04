2baf5643fb0d931d3dae0f4fb14285de0905bbbb
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-22 14:52:23 +0000

    Add function Lucas

--- Lucas/Program.cs
+++ Lucas/Program.cs
@@ -4,6 +4,26 @@ namespace Lucas
 {
     class Program
     {
+        private static int Lucas(int n)
+        {
+            if (n==0)
+            {
+                int number = 1;
+            }
+
+            else if (n==1)
+            {
+                int number = 2;
+            }
+
+            else
+            {
+                int number = (n - 1) + (n - 2);
+            }
+
+            return number;
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
