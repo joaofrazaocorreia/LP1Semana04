e2d4bffad640904e909903144b86347700b69c83
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-22 14:45:06 +0000

    Create project Lucas

---
+++ Lucas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
