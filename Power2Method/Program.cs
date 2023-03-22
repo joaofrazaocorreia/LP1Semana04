250f714521c648e2a9d04ccbf9053f6cb569f689
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-22 13:40:05 +0000

    Create project Power2Method.

---
+++ Power2Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
