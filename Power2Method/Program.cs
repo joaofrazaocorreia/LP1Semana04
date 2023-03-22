206fb7c38dfe5b5d70eacab460f684b545972804
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-22 13:45:05 +0000

    Add function PowersOf2Until5

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,14 @@ namespace Power2Method
 {
     class Program
     {
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
